c825216011632a20a2d3ce27d4f9991fc45216d3
Anand-Prasad-14 <adrock4321anandgp@gmail.com>
2024-06-05 17:26:24 +0530

    operation

--- Javascript-Basics/03_Conversion.js
+++ Javascript-Basics/03_Conversion.js
@@ -17,3 +17,42 @@ let isLoggedIn = 1 // => true
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
 console.log(booleanIsLoggedIn)
+
+//************************Operations******************** */
+
+// let num1 = 10
+// let negNum1 = - num1
+// console.log(negNum1)
+// let num2 = 20
+
+// console.log(num1 + num2)
+// console.log(num1 - num2)
+
+// console.log(num1 * num2)
+
+// console.log(num1 / num2)
+
+// console.log(num1 % num2)
+
+// console.log(num1 ** num2)
+
+
+let str1 = "hello"
+let str2 = "anand"
+let str3 = str1 + str2
+console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+// console.log((3 + 4) * 5 % 3);
+
+console.log(+true);
+console.log(+"");
+
+let num1, num2, num3
+
+let gameCounter = 100
+
+console.log(++gameCounter)
