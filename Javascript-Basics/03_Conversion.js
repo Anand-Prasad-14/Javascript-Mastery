0fbb6742c711c6eb3680e2350c4b132adc8b258f
Anand-Prasad-14 <adrock4321anandgp@gmail.com>
2024-06-05 17:07:05 +0530

    conversion of dataTypes

---
+++ Javascript-Basics/03_Conversion.js
@@ -0,0 +1,19 @@
+let score = "33abc"  // => NaN
+//let score = null => 0
+//let score = true => 1
+//let score = 1
+
+console.log( typeof score)
+console.log(typeof(score))
+
+let valueNumber = Number(score)
+
+console.log(valueNumber)
+console.log(typeof valueNumber)
+
+let isLoggedIn = 1 // => true
+//let isLoggedIn = ""  => false
+//let isLoggedIn = "anand" => true
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
